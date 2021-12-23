c5961b4b3865d0ce15ab99f77a14dcdc03faf8ca
yu432 <77671036+yu432@users.noreply.github.com>
2021-12-23 20:03:07 +0300

    Update script.sh

--- .github/script.sh
+++ .github/script.sh
@@ -3,3 +3,6 @@ OUTPUT=$(docker ps -l -q)
 docker cp "${OUTPUT}":/testing_for_docker/docker-cv/Simple-CV/main.pdf resume.pdf
 ls -l
 git status
+git add resume.pdf
+git commit -m "publishing_resume"
+git push
