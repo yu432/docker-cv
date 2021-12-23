9ad63d4dcd4d16343f15d72787bb5392c2ce5a0b
yu432 <77671036+yu432@users.noreply.github.com>
2021-12-23 20:16:04 +0300

    Update script.sh

--- .github/script.sh
+++ .github/script.sh
@@ -3,6 +3,7 @@ OUTPUT=$(docker ps -l -q)
 docker cp "${OUTPUT}":/testing_for_docker/docker-cv/Simple-CV/main.pdf resume.pdf
 ls -l
 git status
+git remote set-url --push origin https://yu432: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@github.com/yu432/docker-cv
 git add resume.pdf
 git commit -m "publishing_resume"
 git push
