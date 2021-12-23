25d4a7b7e08aac95b74d182351c72b193095cc75
yu432 <77671036+yu432@users.noreply.github.com>
2021-12-23 20:34:44 +0300

    Update script.sh

--- .github/script.sh
+++ .github/script.sh
@@ -8,4 +8,4 @@ git config --global user.name "Yurii Potapov"
 git remote set-url --push origin https://yu432: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@github.com/yu432/docker-cv
 git add resume.pdf
 git commit -m "publishing_resume"
-git push
+git push --force
