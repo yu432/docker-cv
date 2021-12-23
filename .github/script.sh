7bbe1f84c3d525a40195252d449cd393b8336130
yu432 <77671036+yu432@users.noreply.github.com>
2021-12-23 18:53:34 +0300

    Update script.sh

--- .github/script.sh
+++ .github/script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 OUTPUT=$(docker ps -l -q)
-docker cp "${OUTPUT}":/testing_for_docker/docker-cv/Simple-CV/main.pdf neww.pdf
+docker cp "${OUTPUT}":/testing_for_docker/docker-cv/Simple-CV/main.pdf resume.pdf
     
